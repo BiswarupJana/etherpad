e235d9f062723569e47a36f50f2a78a7c3fe7131
Biswarup Jana <biswaruprx21@gmail.com>
2024-09-04 23:47:31 +0530

    etherpad  init

--- etherpad-nextjs/src/app/pads/[padID]/page.tsx
+++ etherpad-nextjs/src/app/pads/[padID]/page.tsx
@@ -1,9 +1,15 @@
-import DocumentEditor from '../../../components/DocumentEditor';
+// import DocumentEditor from '../../../components/DocumentEditor';
 
 export default function PadPage({ params }: { params: { padID: string } }) {
   return (
-    <div>
-      <DocumentEditor padID={params.padID} />
+    <div className="flex flex-col items-center justify-center p-24">
+      {/* <DocumentEditor padID={params.padID} /> */}
+      <iframe
+        src={`http://localhost:9001/p/hvRl54xYBLwAGYtwSkMY`}
+        frameBorder="0"
+        width="70%"
+        height="800px"
+      />
     </div>
   );
 }
